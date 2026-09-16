907b830ef9104a0ca0212dbbc7ccf3b0b959f3fc
FarizaAlzh <a_fariza@kbtu.kz>
2026-09-16 15:08:47 +0500

    prac2

--- Assignment2.playground/Contents.swift
+++ Assignment2.playground/Contents.swift
@@ -1,3 +1,20 @@
-import Cocoa
+// Practice 2 : Working with Collections in Swift
 
-var greeting = "Hello, playground"
+//Easy tasks
+print("Easy tasks: ")
+let fruits: [String] = ["apple" , "banana", "orange", "pear" , "grape"]
+print(fruits[0] ,fruits[1], fruits[2])
+
+var setNumber: Set<Int> = [52, 67, 1, 2, 3]
+setNumber.insert(76)
+print(setNumber)
+
+var dicNames: [String: Int] = ["C++": 1983 , "Python": 1991, "Go":2009]
+dicNames["Swift"] =  2014
+print(dicNames)
+
+var colors: [String] = ["red", "blue", "yellow", "brown"]
+colors[1] = "pink"
+print(colors)
+
+
