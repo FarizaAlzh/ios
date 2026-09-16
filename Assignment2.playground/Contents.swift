74e34d52f846d80611f462a14f6402bc17fad791
FarizaAlzh <a_fariza@kbtu.kz>
2026-09-16 15:19:43 +0500

    prac2

--- Assignment2.playground/Contents.swift
+++ Assignment2.playground/Contents.swift
@@ -3,18 +3,20 @@
 //Easy tasks
 print("Easy tasks: ")
 let fruits: [String] = ["apple" , "banana", "orange", "pear" , "grape"]
-print(fruits[0] ,fruits[1], fruits[2])
+print(fruits[2])
 
 var setNumber: Set<Int> = [52, 67, 1, 2, 3]
 setNumber.insert(76)
 print(setNumber)
 
-var dicNames: [String: Int] = ["C++": 1983 , "Python": 1991, "Go":2009]
-dicNames["Swift"] =  2014
-print(dicNames)
+var dicNames: [String: Int] = ["Swift": 2014 , "Python": 1991, "Go":2009]
+print(dicNames["Swift"])
+print(dicNames["Swift"]!)
 
 var colors: [String] = ["red", "blue", "yellow", "brown"]
 colors[1] = "pink"
 print(colors)
+
+//
 
 
