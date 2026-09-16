f6c8e3572d1fc62808ac3bd890ea2766865832ce
FarizaAlzh <a_fariza@kbtu.kz>
2026-09-16 12:03:04 +0500

    First commit

---
+++ Assignment1.playground/Contents.swift
@@ -0,0 +1,32 @@
+// Practice 1
+
+// Step 1
+let firstName: String = "Fariza"
+let lastName = "Alzhan"
+var age: Int = 21
+let birthYear = 2005
+var isStudent: Bool = true
+var height: Double = 1.65
+
+//bonus
+let currentYear = 2026
+age = currentYear - birthYear
+print(age)
+
+// Step 2
+var hobby: String = "dancing"
+var numberOfHobbies = 3
+let favoriteNumber = 67
+var isHobbyCreative: Bool = true
+
+// Step 3
+var lifeStory = "My name is \(firstName) . I am \(age) years old, born in \(birthYear). I am currently a student. I enjoy \(hobby), which is a creative hobby. I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber)."
+
+//Step 4
+print(lifeStory)
+
+//Bonus
+
+
+
+
